a5c7b2f0c0306cacb34cee2071626b2957746fa1
Limonsso <ezechiel.gnepa09@gmail.com>
2022-01-23 17:26:48 -0500

    🚧 WIP

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,5 +1,6 @@
 export class GlobalConstants {
-  //public static reatEstateApiBaseUrl: string = "https://localhost:5003/api"
-  public static reatEstateApiBaseUrl: string = "https://localhost:44391/api"
+  public static reatEstateApiBaseUrl: string = "https://localhost:5003/api"
+  // public static reatEstateApiBaseUrl: string = "https://localhost:44391/api"
   public static mortgageCalculatorApiBaseUrl: string = "http://192.168.2.21:5001/api"
+  //public static reatEstateApiBaseUrl: string = "http://192.168.2.21:5003/api"
 }
