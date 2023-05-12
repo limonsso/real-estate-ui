806fa5a4ff23c5a2e5eb3e217517a5c1c8581dca
Ézéchiel Gnepa <ezechiel.gnepa09@gmail.com>
2023-05-12 17:05:16 -0400

    update url backend

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
-  public static reatEstateApiBaseUrl: string = "https://realestate-web-api.herokuapp.com/api"
-  // public static reatEstateApiBaseUrl: string = "https://localhost:44391/api"
-  public static mortgageCalculatorApiBaseUrl: string = "https://realestate-calculator-api.herokuapp.com//api"
+  //public static reatEstateApiBaseUrl: string = "https://realestate-web-api.herokuapp.com/api"
+  public static reatEstateApiBaseUrl: string = "https://localhost:44391/api/v1"
+  public static mortgageCalculatorApiBaseUrl: string = "http://localhost:5001/api"
   //public static reatEstateApiBaseUrl: string = "http://192.168.2.21:5003/api"
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
-import {GlobalConstants} from "../common/global-constants";
-import {HttpClient} from "@angular/common/http";
-import {BehaviorSubject, Observable} from "rxjs";
-import {User} from "../models/user";
-import {map} from "rxjs/operators";
-import {LocalStorageService} from "angular-web-storage";
-import {UserTypeEnum} from "../models/UserType.enum";
-import {FavoriteProperty} from "../models/favorite-property";
+import { Injectable } from '@angular/core';
+import { GlobalConstants } from "../common/global-constants";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "../models/user";
+import { map } from "rxjs/operators";
+import { LocalStorageService } from "angular-web-storage";
+import { UserTypeEnum } from "../models/UserType.enum";
+import { FavoriteProperty } from "../models/favorite-property";
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class UserService {
 
   constructor(private readonly http: HttpClient, private readonly localStorage: LocalStorageService) {
     this.currentUserSubject = new BehaviorSubject<User | null>(this.localStorage.get('currentUser'));
-    if(this.currentUserValue === null){
+    if (this.currentUserValue === null) {
       this.initGuestUser()
     }
     this.currentUser = this.currentUserSubject.asObservable();
@@ -30,7 +30,7 @@ export class UserService {
 
   login(email: string, password: string): Observable<User> {
     const url = this.apiBaseUrl + "/user/login"
-    return this.http.post<User>(url, {email: email, password: password}).pipe(map(user => {
+    return this.http.post<User>(url, { email: email, password: password }).pipe(map(user => {
       // store user details and jwt token in local storage to keep user logged in between page refreshes
       user.userType = UserTypeEnum.Connected
       this.localStorage.set('currentUser', user, 30, 'm');
@@ -39,21 +39,20 @@ export class UserService {
     }));
   }
 
-  private initGuestUser()
-  {
-    var guestUser = new User(0,"","",[],UserTypeEnum.Guest)
+  private initGuestUser() {
+    var guestUser = new User(0, "", "", [], UserTypeEnum.Guest)
     this.localStorage.set('currentUser', guestUser, 30, 'm');
     this.currentUserSubject.next(guestUser);
   }
 
   getLocalitiesSearched(): Observable<string[]> {
-    const url = this.apiBaseUrl + "/user/getlocalitiessearched"
+    const url = this.apiBaseUrl + "/user/localities-searched"
     return this.http.get<string[]>(url);
   }
 
   RefreshUserConnectedData() {
-    const url = this.apiBaseUrl + "/user/userdata"
-    this.http.get<User>(url).subscribe(user =>{
+    const url = this.apiBaseUrl + "/user/info"
+    this.http.get<User>(url).subscribe(user => {
       user.userType = UserTypeEnum.Connected
       this.localStorage.set('currentUser', user, 30, 'm');
       this.currentUserSubject.next(user);
@@ -61,36 +60,36 @@ export class UserService {
   }
 
   updateLocalitiesSearched(localitiesSelected: string[]) {
-    if(this.currentUserValue.userType == UserTypeEnum.Guest){
+    if (this.currentUserValue.userType == UserTypeEnum.Guest) {
       this.updateLocalitiesSearchedUser(localitiesSelected);
-    }else{
-      const url = this.apiBaseUrl + "/user/updatelocalitiessearch"
-      this.http.post<string[]>(url, localitiesSelected).subscribe(localities=>{
+    } else {
+      const url = this.apiBaseUrl + "/user/localities-search/update"
+      this.http.post<string[]>(url, localitiesSelected).subscribe(localities => {
         this.updateLocalitiesSearchedUser(localitiesSelected);
       });
     }
   }
 
   saveFavoriteProperty(favoriteProperty: FavoriteProperty) {
-    if(this.currentUserValue.userType == UserTypeEnum.Connected){
-      const url = this.apiBaseUrl + "/user/savefavoriteproperty"
-      this.http.post<FavoriteProperty>(url, favoriteProperty).subscribe(favorite=>{
+    if (this.currentUserValue.userType == UserTypeEnum.Connected) {
+      const url = this.apiBaseUrl + "/user/favoritep-roperty/save"
+      this.http.post<FavoriteProperty>(url, favoriteProperty).subscribe(favorite => {
         this.RefreshUserConnectedData()
       });
     }
   }
 
-  deleteFavoriteProperty(propertyId:string) {
-    if(this.currentUserValue.userType == UserTypeEnum.Connected){
-      const url = this.apiBaseUrl + "/user/deletefavoriteproperty?PropertyId="+propertyId
-      this.http.delete(url).subscribe(response=>{
+  deleteFavoriteProperty(propertyId: string) {
+    if (this.currentUserValue.userType == UserTypeEnum.Connected) {
+      const url = this.apiBaseUrl + "/user/favorite-property/delete/?PropertyId=" + propertyId
+      this.http.delete(url).subscribe(response => {
         this.RefreshUserConnectedData()
       });
     }
   }
 
   updateLocalitiesSearchedUser(localitiesSelected: string[]) {
-    let user =this.localStorage.get('currentUser')
+    let user = this.localStorage.get('currentUser')
     user.localitiesSelectedLastSearch = localitiesSelected;
     this.localStorage.set('currentUser', user, 30, 'm');
     this.currentUserSubject.next(user);
